c8c0b161dbd7cadec14c57f50345fbf037d130e0
tikiatua <tikiatua@gmail.com>
2018-01-05 14:16:16 +0100

    add some comments to the startup scripts

--- build/scripts/client.sh
+++ build/scripts/client.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# start the client service on port 8081 and connect to the grpc service
+# via internal envoy proxy
 /app/bin/client --port=8081 --grpc=127.0.0.1:9001 &
-/usr/local/bin/envoy -c /app/envoy/client.json --v2-config-only -l info
+
+# start the envoy proxy with the given configuration (in v2 api) and log
+# only level error and above
+/usr/local/bin/envoy -c /app/envoy/client.json --v2-config-only -l error
