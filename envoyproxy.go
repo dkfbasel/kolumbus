7e655df74acb4199ae6858a9f7ec637203ceb2d1
tikiatua <tikiatua@gmail.com>
2018-02-12 17:14:09 +0100

    start envoyproxy as part of kolumbus

---
+++ envoyproxy.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/pkg/errors"
+)
+
+// StartEnvoyproxy will initialize an envoy proxy that can be used to connect
+// services locally or with a remote service (local services will be prioritized)
+func (dns *Kolumbus) StartEnvoyproxy(errs chan error) {
+
+	// command to start envoy
+	// /usr/local/bin/envoy -c /app/envoy/config.json --v2-config-only -l error
+
+	go func() {
+		bin := "/usr/local/bin/envoy"
+		args := []string{"-c", "/app/envoy/config/proxy.json", "--v2-config-only", "-l error"}
+
+		cmd := exec.Command(bin, args...)
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Run()
+		if err != nil {
+			errs <- errors.Wrap(err, "could not start envoy proxy")
+		}
+	}()
+
+}
