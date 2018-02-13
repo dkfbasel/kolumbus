c2231528b2fade3921a069a797817484dd9e866b
tikiatua <tikiatua@gmail.com>
2018-02-13 16:11:50 +0100

    adapt build script

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,5 @@
 # build the kolumbus binary
 cd ..
 gox -osarch="linux/amd64" -output="./build/kolumbus"
-cp ./build/kolumbus ./examples/grpc/build/bin/kolumbus
+cp ./build/kolumbus ./examples/grpc-prod/build/bin/kolumbus
 cd ./build
