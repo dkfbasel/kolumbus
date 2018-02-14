6cd8706af04c167c75f86079494f1c8501655c61
tikiatua <tikiatua@gmail.com>
2018-02-14 09:08:42 +0100

    parse configuration

--- main.go
+++ main.go
@@ -2,9 +2,23 @@
 // enables envoyproxy to create a dynamic microservice mesh
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/namsral/flag"
+)
 
 func main() {
+
+	// initialize the configuration
+	config := Config{}
+
+	flag.IntVar(&config.DataPlanePort, "dataplane", 1492, "port to start the envoyproxy data plane discovery service on")
+	flag.IntVar(&config.LocalProxyPort, "localproxy", 1494, "port to start local proxy service of the internal envoyproxy instance on")
+	flag.StringVar(&config.RemoteProxyMode, "remote-mode", "none", "modus to use for remote proxy service (none/outbound/inbound)")
+	flag.IntVar(&config.RemoteProxyPort, "remote-port", 1498, "(inbound) port to start the remote proxy service on the internal envoyproxy instance on")
+	flag.StringVar(&config.RemoteProxyAddress, "remote-address", "", "(outbound) address of the remote proxy service to call")
+	flag.Parse()
 
 	kolumbus := FindABraveNewWorld()
 
@@ -17,12 +31,12 @@ func main() {
 
 	// start an envoyproxy and optionally open a port for
 	// external communication
-	kolumbus.StartEnvoyproxy(1494, 1498, errorChan)
+	kolumbus.StartEnvoyproxy(config, errorChan)
 	log.Println("- envoy proxy started")
 
 	// start a data plane discovery service for envoyproxies to automatically
 	// create a service mesh
-	kolumbus.StartEnvoyDataPlaneServer(1492, errorChan)
+	kolumbus.StartEnvoyDataPlaneServer(config.DataPlanePort, errorChan)
 	log.Println("- envoy discovery server started")
 
 	// log any errors
@@ -36,4 +50,23 @@ func main() {
 	// proxy on remote machine:
 	// host and port of remote address
 
+}
+
+// Config is used to define customizable configuration options
+type Config struct {
+	// define a port for envoy proxy data plane server that will be checked
+	// by all envoyproxy instances for configuration information
+	DataPlanePort int
+
+	// define a port for a local envoyproxy instance that will be used
+	// as local proxy for development
+	LocalProxyPort int
+
+	// define a port for a local envoyproxy instance that will be used for
+	// remote connections to the cluster. connections are secured with tls
+	// the mode (inbound/outbound) will define if envoyproxy will listen to
+	// requests or send out requests to a remote cluster
+	RemoteProxyMode    string // inbound or outbound
+	RemoteProxyPort    int    // port to start the remote service on (inbound)
+	RemoteProxyAddress string // address for a remote service to call (outbound)
 }
