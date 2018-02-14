e199222a9c0d1ebacb1f0538631a08526721e839
tikiatua <tikiatua@gmail.com>
2018-02-14 09:43:30 +0100

    adapt build script to build docker container

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
 # build the kolumbus binary
-cd ..
-gox -osarch="linux/amd64" -output="./build/kolumbus"
-cp ./build/kolumbus ./examples/grpc-prod/build/bin/kolumbus
-cd ./build
+echo "-- build kolumbus binary (y/n)"
+read buildBinary
+if [ "$buildBinary" = "y" ] || [ "$buildBinary" = "" ]; then
+  cd ..
+  gox -osarch="linux/amd64" -output="./build/kolumbus"
+  cp ./build/kolumbus ./examples/grpc-prod/build/bin/kolumbus
+  cd ./build
+fi
+
+echo "-- build docker container (y/n):"
+read buildContainer
+
+if [ "$buildContainer" = "y" ] || [ "$buildContainer" = "" ]; then
+  echo "specify container tag (dev):"
+  read tag
+  if [ "$tag" = "" ]; then
+    tag="dev"
+  fi
+  docker build -t dkfbasel/kolumbus:$tag .
+
+	echo "-- push docker container (y/n)"
+	read pushContainer
+	if [ "$pushContainer" = "y" ] || [ "$pushContainer" = "" ]; then
+	  docker push dkfbasel/kolumbus:$tag
+	fi
+
+fi
